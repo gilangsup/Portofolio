8d7d0a82cdba976a7eb3f870ba72a6766ec64016
gilangsup <gilangsprb@gmail.com>
2023-01-19 14:30:58 +0700

    added new tools

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='text-white flex justify-center'>
+      <div className='my-8 flex'>
+      <p className='px-10'>Profile</p>
+      <p className='px-10'>Projects</p>
+      <p className='px-10'>Skills</p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
