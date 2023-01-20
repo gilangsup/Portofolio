ee077d378adc5de10a8dd9cb27c7988af76d88da
gilangsup <gilangsprb@gmail.com>
2023-01-20 23:58:26 +0700

    added transform motion

---
+++ src/components/TextSpan.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import {motion, useAnimationControls} from 'framer-motion'
+
+
+const TextSpan = ({children}) => {
+
+    const controls = useAnimationControls()
+    const [isplaying,setIsplaying] = useState(false)
+
+    const rubberBand = () => {
+       controls.start({
+           transform: [
+               "scale3d(1, 1, 1)",
+               "scale3d(1.4, .55,1)",
+               "scale3d(.75, 1.25, 1)",
+               "scale3d(1.25, .85, 1)",
+               "scale3d(.9, 1.05, 1)",
+               "scale3d(1, 1, 1)",
+           ],
+           times: [0, .4, .6, .7, .8, .9]
+       })
+       setIsplaying(true)
+    }
+
+  return (
+      <motion.span
+       animate={controls}
+       onMouseOver={() => {if(!isplaying)rubberBand()}}
+       onAnimationComplete={() => setIsplaying(false)}
+       className='text-5xl inline-block'>
+      {children}
+    </motion.span>
+  )
+}
+
+export default TextSpan
